3e8e86bd1dd860cb8cc668c4bd0362e3008eeef4
troygamurot <troygamurot@cs104u-2a.acad.fullerton.edu>
2020-01-30 14:53:39 -0800

    Hello World Plus Plus

--- part-2/hello_world.cpp
+++ part-2/hello_world.cpp
@@ -1,11 +1,18 @@
-// Oscar Mayer
-// CPSC 121-99
-// oscar.mayer@csu.fullerton.edu
+// Troy Gamurot
+// CPSC 121-03
+// troygamurot@csu.fullerton.edu
 
+#include <iomanip>
 #include <iostream>
 using namespace std;
 
-int main(int argc, char *argv[]){
-  cout << "Hello world!" << endl;
+int main(int argc, char*argv[]){
+  int numprint = stoi(argv[2]);
+  if (argc < 2)
+    cout<<"Too few arguments"<<endl;
+  else
+    for (int i = 0; i < numprint; i++){
+      cout<<"Hello "<<argv[1]<<endl;
+    }
   return 0;
 }
